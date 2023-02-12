4a108e22edb1ec3604807496cb8f0611b4acc9f5
Thomas Willson <thomas.willson@staflsystems.com>
2023-02-12 13:22:46 -0800

    another attempt at fixing CI

--- tests/test_git.py
+++ tests/test_git.py
@@ -1,32 +1,29 @@
 import pytest
+import tempfile
 from os import system
-from pathlib import Path
-from shutil import rmtree
 
 from staflversion.git import GitWrapper
 
 
 @pytest.fixture
 def sample_repo():
-    rmtree("sample_repo", ignore_errors=True)
-    Path.mkdir("sample_repo")
-    system(
-        "cd sample_repo &&\
-        git init &&\
-        git config user.name 'test' &&\
-        git config user.email 'test@test.com' &&\
-        git commit --allow-empty -m c1 &&\
-        git tag 0.0.0+1 &&\
-        git commit --allow-empty -m c2 &&\
-        git tag invalid_tag &&\
-        git commit --allow-empty -m c3 &&\
-        git tag 0.0.0+2 &&\
-        git commit --allow-empty -m c4 &&\
-        git commit --allow-empty -m c5\
-    "
-    )
-    yield GitWrapper("sample_repo")
-    rmtree("sample_repo")
+    with tempfile.TemporaryDirectory() as tempdir:
+        system(
+            f"cd {tempdir} &&\
+            git init &&\
+            git config user.name 'test' &&\
+            git config user.email 'test@test.com' &&\
+            git commit --allow-empty -m c1 &&\
+            git tag 0.0.0+1 &&\
+            git commit --allow-empty -m c2 &&\
+            git tag invalid_tag &&\
+            git commit --allow-empty -m c3 &&\
+            git tag 0.0.0+2 &&\
+            git commit --allow-empty -m c4 &&\
+            git commit --allow-empty -m c5\
+        "
+        )
+        yield GitWrapper(tempdir)
 
 
 def test_get_tags(sample_repo: GitWrapper):
