2a8ac0d34307ebfdefb2bf972d87c633d1667883
Thomas Willson <thomas.willson@staflsystems.com>
2023-02-12 14:09:45 -0800

    testing again

--- tests/test_git.py
+++ tests/test_git.py
@@ -1,30 +1,37 @@
 import pytest
 import tempfile
 from pytest import MonkeyPatch
-from os import system
+from os import system, getcwd
 
 from staflversion.git import GitWrapper
 
 
 @pytest.fixture
-def sample_repo(monkeypatch: MonkeyPatch):
+def tempdir():
     with tempfile.TemporaryDirectory() as tempdir:
-        monkeypatch.chdir(tempdir)
-        system(
-            "git init &&\
-            git config user.name 'test' &&\
-            git config user.email 'test@test.com' &&\
-            git commit --allow-empty -m c1 &&\
-            git tag 0.0.0+1 &&\
-            git commit --allow-empty -m c2 &&\
-            git tag invalid_tag &&\
-            git commit --allow-empty -m c3 &&\
-            git tag 0.0.0+2 &&\
-            git commit --allow-empty -m c4 &&\
-            git commit --allow-empty -m c5\
-        "
-        )
-        yield GitWrapper(tempdir)
+        yield tempdir
+        print(f"clean dir: {getcwd()}")
+
+
+@pytest.fixture
+def sample_repo(tempdir: str, monkeypatch: MonkeyPatch):
+    # tempdir above must come before monkeypatch so that the directory is changed back before the directory is cleanup up
+    monkeypatch.chdir(tempdir)
+    system(
+        "git init &&\
+        git config user.name 'test' &&\
+        git config user.email 'test@test.com' &&\
+        git commit --allow-empty -m c1 &&\
+        git tag 0.0.0+1 &&\
+        git commit --allow-empty -m c2 &&\
+        git tag invalid_tag &&\
+        git commit --allow-empty -m c3 &&\
+        git tag 0.0.0+2 &&\
+        git commit --allow-empty -m c4 &&\
+        git commit --allow-empty -m c5\
+    "
+    )
+    yield GitWrapper(tempdir)
 
 
 def test_get_tags(sample_repo: GitWrapper):
