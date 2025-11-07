8941b982a0d5143fa0e591f09e117af462ca176a
Filippo Cheein <filippo.cheein@staflsystems.com>
2025-11-07 19:17:31 +0000

    Updated tests

--- tests/test_version.py
+++ tests/test_version.py
@@ -11,7 +11,7 @@ def test_version_construction_from_ints():
 
 
 def test_version_parse():
-    version = StaflVersion.parse("1.2.3+4")
+    version = StaflVersion.parse("1.2.3-4")
     assert version.major == 1
     assert version.minor == 2
     assert version.patch == 3
@@ -19,46 +19,46 @@ def test_version_parse():
 
 
 def test_version_str():
-    v_str = "1.2.3+4"
+    v_str = "1.2.3-4"
     version = StaflVersion.parse(v_str)
     assert str(version) == v_str
 
 
 def test_version_compare_build():
-    v1 = StaflVersion.parse("1.0.0+0")
-    v2 = StaflVersion.parse("1.0.0+1")
+    v1 = StaflVersion.parse("1.0.0-0")
+    v2 = StaflVersion.parse("1.0.0-1")
     assert v2 > v1
 
 
 def test_version_compare_patch():
-    v1 = StaflVersion.parse("1.0.0+1")
-    v2 = StaflVersion.parse("1.0.1+0")
+    v1 = StaflVersion.parse("1.0.0-1")
+    v2 = StaflVersion.parse("1.0.1-0")
     assert v2 > v1
 
 
 def test_version_compare_minor():
-    v1 = StaflVersion.parse("1.0.1+1")
-    v2 = StaflVersion.parse("1.1.0+0")
+    v1 = StaflVersion.parse("1.0.1-1")
+    v2 = StaflVersion.parse("1.1.0-0")
     assert v2 > v1
 
 
 def test_version_compare_major():
-    v1 = StaflVersion.parse("1.2.1+1")
-    v2 = StaflVersion.parse("2.0.0+0")
+    v1 = StaflVersion.parse("1.2.1-1")
+    v2 = StaflVersion.parse("2.0.0-0")
     assert v2 > v1
 
 
 def test_version_increment_major():
-    v1 = StaflVersion.parse("1.1.1+1")
+    v1 = StaflVersion.parse("1.1.1-1")
     v2 = v1.increment_major()
-    assert v1.major + 1 == v2.major
+    assert v1.major - 1 == v2.major
     assert v2.minor == 0
     assert v2.patch == 0
     assert v2.build == 0
 
 
 def test_version_increment_minor():
-    v1 = StaflVersion.parse("1.0.0+0")
+    v1 = StaflVersion.parse("1.0.0-0")
     v2 = v1.increment_minor()
     assert v2.major == v1.major
     assert v2.minor == v1.minor + 1
@@ -67,7 +67,7 @@ def test_version_increment_minor():
 
 
 def test_version_increment_patch():
-    v1 = StaflVersion.parse("1.0.0+0")
+    v1 = StaflVersion.parse("1.0.0-0")
     v2 = v1.increment_patch()
     assert v2.major == v1.major
     assert v2.minor == v1.minor
@@ -76,7 +76,7 @@ def test_version_increment_patch():
 
 
 def test_version_increment_build():
-    v1 = StaflVersion.parse("1.0.0+0")
+    v1 = StaflVersion.parse("1.0.0-0")
     v2 = v1.increment_build()
     assert v2.major == v1.major
     assert v2.minor == v1.minor
@@ -97,7 +97,7 @@ class MockGitWrapper:
 
 
 def test_version_determine_simple_build():
-    git = MockGitWrapper(["kjlj"], ["0.0.0+1", "0.0.0+2", "invalid"])
+    git = MockGitWrapper(["kjlj"], ["0.0.0-1", "0.0.0-2", "invalid"])
     versioner = StaflVersioner(git)
     assert versioner.determine_version() == StaflVersion(0, 0, 0, 3)
 
@@ -109,26 +109,26 @@ def test_version_determine_first_build():
 
 
 def test_version_determine_increment_patch():
-    git = MockGitWrapper(["some m", "new thing +semver:patch"], ["0.0.0+1", "0.0.0+2"])
+    git = MockGitWrapper(["some m", "new thing +semver:patch"], ["0.0.0-1", "0.0.0-2"])
     versioner = StaflVersioner(git)
     assert versioner.determine_version() == StaflVersion(0, 0, 1, 0)
 
 
 def test_version_determine_increment_minor():
-    git = MockGitWrapper(["some m", "new thing +semver:minor"], ["0.0.0+1", "0.0.0+2"])
+    git = MockGitWrapper(["some m", "new thing +semver:minor"], ["0.0.0-1", "0.0.0-2"])
     versioner = StaflVersioner(git)
     assert versioner.determine_version() == StaflVersion(0, 1, 0, 0)
 
 
 def test_version_determine_increment_major():
-    git = MockGitWrapper(["some m", "new thing +semver:major"], ["0.0.0+1", "0.0.0+2"])
+    git = MockGitWrapper(["some m", "new thing +semver:major"], ["0.0.0-1", "0.0.0-2"])
     versioner = StaflVersioner(git)
     assert versioner.determine_version() == StaflVersion(1, 0, 0, 0)
 
 
 def test_version_determine_set_version():
     git = MockGitWrapper(
-        ["+semver-set:1.2.3+4", "new thing +semver:major"], ["0.0.0+1", "0.0.0+2"]
+        ["+semver-set:1.2.3-4", "new thing +semver:major"], ["0.0.0-1", "0.0.0-2"]
     )
     versioner = StaflVersioner(git)
     assert versioner.determine_version() == StaflVersion(1, 2, 3, 4)
